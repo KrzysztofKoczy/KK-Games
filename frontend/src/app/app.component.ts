18be9fabaab3d7ae0db805f81b613f3bdaf479b3
Krzysztof <krzysztof.koczy99@gmail.com>
2025-11-09 16:40:02 +0100

    add black mode

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonHeader, IonRouterOutlet, IonTitle, IonToggle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
-  imports: [IonApp, IonRouterOutlet, RouterOutlet],
-  template: `
-    <ion-app>
-      <ion-router-outlet/>
-    </ion-app>
-  `
+  imports: [IonApp, IonRouterOutlet, RouterOutlet, IonToggle, IonToolbar, IonTitle, IonHeader],
+  templateUrl: './app.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  darkMode = signal(false);
   title = 'Games App';
+
+  constructor() {
+    effect(() => {
+      const isDark = this.darkMode();
+      const htmlElement = document.documentElement;
+
+      isDark ? htmlElement.classList.add('ion-palette-dark') : htmlElement.classList.remove('ion-palette-dark');
+
+      localStorage.setItem('darkMode', String(isDark));
+    });
+  }
+
+  ngOnInit() {
+    this.getModePreference();
+  }
+
+
+  toggleDarkMode() {
+    this.darkMode.set(!this.darkMode());
+  }
+
+  private getModePreference() {
+    if (localStorage.getItem('darkMode') !== null) {
+      this.darkMode.set(localStorage.getItem('darkMode') === 'true');
+    } else {
+      this.darkMode.set(window.matchMedia('(prefers-color-scheme: dark)').matches);
+    }
+  }
 }
 
